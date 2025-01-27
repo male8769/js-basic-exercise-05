c580160c6e7003061d71dc4192740b4ba6f3c9f1
male8769 <malene_storgaard@live.dk>
2025-01-27 10:15:26 +0100

    yep

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+document.getElementById("text").textContent = `This text has been updated with JavaScript!`;
+
+
+
+// ## JavaScript Task:
+
+// 1. Link to the external JavaScript file placed in the `js` folder:
+//    ```html
+//    <script src="js/script.js"></script>
+//    ```
+// 2. Ensure "use strict" is at the top of your script.js file.
+
+// 3. Create a `<p>` tag in your HTML with:
+//    An id attribute set to `text`.
+//    The text "old content" written between the `<p>` opening and closing tags
+//       ```html
+//       <p id="text">old content</p>
+//       ```
+
+// 4. Use `document.getElementById` inside your script.js to select the `<p>` tag by its `id` attribute.
+
+// 5. Use `textContent` to update the text of the <p> tag to:
+
+//    ```html
+//    "This text has been updated with JavaScript!"
+//    ```
+
+// 6. Open **Go Live** in your VS Code editor and check that the content has been updated.
